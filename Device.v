f11dda21317b70124ac470dc6eeeef18f81c2dd4
hosniadel666 <hosniadel666>
2020-12-16 09:10:33 +0200

    First Commit

---
+++ Device.v
@@ -0,0 +1,23 @@
+/*###############################################
+#               DEVICE MODULE                   #
+################################################*/
+
+module Device(
+FRAME,  // Transaction Frame
+CLK, // Clock
+RST, // Reset(Active Low)
+AD, // Address and Data line time multiplexing 
+CBE, // Control command
+IRDY, // Initiator ready(Active Low)
+TRDY, // Target ready(Active Low)
+DEVSEL // Device select ready(Active Low)
+);
+
+endmodule
+
+
+
+
+
+
+        
