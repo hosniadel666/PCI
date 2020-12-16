264294a124495ef98b0b2ddc4d85052bc2475f78
hosniadel666 <hosniadel666>
2020-12-16 09:15:34 +0200

    Add Device_tb module structure

--- Device_tb.v
+++ Device_tb.v
@@ -3,6 +3,40 @@
 ################################################*/
 
 module DEVICE_tb();
+/*################## REG ##################*/
+reg CLK;
+reg RST;
+reg FRAME;
+reg [3:0] CBE;
+wire IRDY;
+/*################## NET ##################*/
+wire [31: 0] AD;
 
+wire TRDY;
+wire DEVSEL;
+
+
+
+
+Device D1(); // by position
+
+initial 
+begin: GENERATE_CLK 
+    clk <= 1;
+    always
+    begin 
+        #1 // f = ??
+        clk <= ~clk;
+    end
+end
+
+
+initial 
+begin
+/*################ TEST WRITE OPERATION #################*/
+
+/*################ TEST READ OPERATION #################*/
+
+end
 endmodule
 
