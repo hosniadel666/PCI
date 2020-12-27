973cbc785c121ae10eb816d53f75a995e391191f
Hassan El-shazly <hassanelshazly2010@gmail.com>
2020-12-27 03:47:53 +0200

    Add STOP/Disconnect with data/Disconnect without data

--- new_device.v
+++ new_device.v
@@ -24,13 +24,14 @@
 
 
 module Device(FRAME,
-              CLK,     // Clock
-              REST,    // Asyncronous Reset(Active Low)
-              AD,      // Address and Data line time multiplexing
-              CBE,     // Control command
-              IRDY,    // Initiator ready(Active Low)
-              TRDY,    // Target ready(Active Low)
-              DEVSEL); // Device select ready(Active Low)
+              CLK,    // Clock
+              REST,   // Asyncronous Reset(Active Low)
+              AD,     // Address and Data line time multiplexing
+              CBE,    // Control command
+              IRDY,   // Initiator ready(Active Low)
+              TRDY,   // Target ready(Active Low)
+              DEVSEL,
+              STOP);
     
     /******************* INPUT ********************/
     input wire CLK;
@@ -42,6 +43,7 @@ module Device(FRAME,
     /******************* OUTPUT *******************/
     output wire TRDY;
     output wire DEVSEL;
+    output wire STOP;
     
     /******************* INOUT ********************/
     inout [31:0] AD;
@@ -54,16 +56,9 @@ module Device(FRAME,
     /****************** INTERNAL ******************/
     reg [31:0] MEM [0:3]; // Device internal memory 4 words
     reg [31:0] TEMP_BUFFER [0:3]; // Device internal buffer 4 words
-    reg [2:0]  INDEX;  // used as a pointer
-    reg [2:0]  INDEX_READ;
-    reg DEVICE_READY;
-    /*
-     initial
-     begin
-     $readmemh("ram.mem", MEM);
-     end
-     */
-    
+    reg [2:0]  INDEX_WRITE;  // used as pointer in case of write operation 
+    reg [2:0]  INDEX_READ;  // used as pointer in case of read operation
+    reg DEVICE_READY;  // used by the device to state that it's not ready
     
     // Keep track of the transations on the bus
     // TRANSATION should be asserted up if there is
@@ -87,31 +82,87 @@ module Device(FRAME,
                 1'b1: TRANSATION <= ~TRANSATION_END;
             endcase
     end
-    
+
     // Save the address & the command in an internal buffers
-    // TODO: Shoud be modified to map to the internal memery addres
     reg [31:0] ADRESS_BUFF;
     reg [3:0] COMMAND_BUFF;
     reg FIRST_DATA_PHASE;
-    reg SECOND_DATA_PHASE;
     always @(posedge CLK)
     begin
-        if (TRANSATION_START)
-        begin
+        if (TRANSATION_START) begin
             ADRESS_BUFF <= AD;
             // We work on 32bit aligend address so we ignore
             // the first two bits
-            // INDEX         <= AD[3:2];
+            // INDEX_WRITE         <= AD[3:2];
             COMMAND_BUFF     <= CBE;
             FIRST_DATA_PHASE <= 1;
-            end else begin
+        end
+        else begin
             FIRST_DATA_PHASE <= 0;
         end
     end
     
-    always @(posedge CLK) begin
-        SECOND_DATA_PHASE <= FIRST_DATA_PHASE;
+    
+    // check of supported brust modes
+    reg TARGET_ABORT;
+    always @(posedge CLK or negedge REST) begin
+        if (~REST) begin
+            TARGET_ABORT <= 0;
+        end
+        else begin
+            if (TRANSATION_START & (AD[1:0] != 2'b00))
+            // unsupported burst mode
+            // Disconnect with data
+                TARGET_ABORT <= 1;
+            else 
+                case (TARGET_ABORT)
+                    1'b0:  TARGET_ABORT <= 0;
+                    1'b1: if (TRANSATION_END)
+                    TARGET_ABORT <= 1'b0;
+                endcase  
+        end
     end
+
+    // Disconnet with data 
+    // in case of write operation delay one cycle to write the first data
+    // in case of read operation delay one cycle to write the first data
+    // it's delayed another cycle becasue of the turnaround cycle
+    reg WRITE_ONE;
+    reg READ_ONE;
+    always @(posedge CLK or negedge REST) begin
+         if (~REST) begin
+             WRITE_ONE <= 1;
+        READ_ONE <= 1;
+        end
+        else begin
+        WRITE_ONE <= ~TARGET_ABORT;
+        READ_ONE <= WRITE_ONE;
+        end
+    end
+
+
+    // Disconnect without data in case of unsupported opeartion
+    reg DISCONNECT_WITHOUT_DATA;
+    always @(posedge CLK or negedge REST) begin
+        if (~REST) begin
+            DISCONNECT_WITHOUT_DATA <= 0;
+        end
+        else begin
+            // TODO: Modify to all unsupported operations
+            if (TRANSATION_START & (CBE[1:0] == 2'b0000))
+            // unsupported operation
+            // Disconnect with data
+                DISCONNECT_WITHOUT_DATA <= 1;
+            else
+                case (DISCONNECT_WITHOUT_DATA)
+                    1'b0: DISCONNECT_WITHOUT_DATA <= 0;
+                    1'b1: if (TRANSATION_END)
+                    DISCONNECT_WITHOUT_DATA <= 1'b0;
+                endcase
+        end
+    end
+
+    
     
     // See if our device is the target
     // (The half clock cycle before the transation as TRANSATION_START)
@@ -135,6 +186,8 @@ module Device(FRAME,
             endcase
     end
     
+    wire COMMOND_READ  = (COMMAND_BUFF == READ_OP);
+    wire COMMOND_WRITE = (COMMAND_BUFF == WRITE_OP);
     // Keep track of the last byte to transfer
     // when the frame asserted up
     wire LAST_DATA_TRANSFER = FRAME & ~IRDY & ~TRDY;
@@ -150,7 +203,7 @@ module Device(FRAME,
         else
             case(TRANSATION)
                 1'b0: DEVSEL_BUFF <= TARGETED; // fast
-                1'b1: DEVSEL_BUFF <= DEVSEL_BUFF & ~LAST_DATA_TRANSFER;
+                1'b1: DEVSEL_BUFF <= DEVSEL_BUFF & ~LAST_DATA_TRANSFER & ~FRAME;
             endcase
     end
     
@@ -171,20 +224,24 @@ module Device(FRAME,
     // Asserting on negtive edge
     reg TRDY_BUFF_NEG;
     reg DEVSEL_BUFF_NEG;
+    reg TARGET_ABORT_NEG;
     always @(negedge CLK or negedge REST) begin
         if (~REST) begin
-            TRDY_BUFF_NEG   <= 0;
-            DEVSEL_BUFF_NEG <= 0;
+            TRDY_BUFF_NEG    <= 0;
+            DEVSEL_BUFF_NEG  <= 0;
+            TARGET_ABORT_NEG <= 0;
         end
         else begin
-            TRDY_BUFF_NEG   <= TRDY_BUFF & DEVICE_READY;
-            DEVSEL_BUFF_NEG <= DEVSEL_BUFF;
+            TRDY_BUFF_NEG    <= TRDY_BUFF & DEVICE_READY & ~DISCONNECT_WITHOUT_DATA;
+            DEVSEL_BUFF_NEG  <= DEVSEL_BUFF;
+            TARGET_ABORT_NEG <= TARGET_ABORT & DISCONNECT_WITHOUT_DATA;
         end
     end
     
     // Asserting DEVSEL & TRDY down during the transaction or tri-state it
     assign DEVSEL = DEVICE_TRANSATION ? ~DEVSEL_BUFF_NEG : 1'bZ;
     assign TRDY   = DEVICE_TRANSATION ? ~TRDY_BUFF_NEG : 1'bZ;
+    assign STOP   = DEVICE_TRANSATION ? ~TARGET_ABORT_NEG : 1'bZ;
     
     
     // Mask to be used with write opeartion
@@ -192,8 +249,8 @@ module Device(FRAME,
     
     
     // Siganls to track the current operation
-    wire DATA_WRITE = ~DEVSEL & (WRITE_OP == COMMAND_BUFF) & ~IRDY;
-    wire DATA_READ  = ~DEVSEL & (READ_OP == COMMAND_BUFF) & ~IRDY & ~TRDY;
+    wire DATA_WRITE = ~DEVSEL & (WRITE_OP == COMMAND_BUFF) & ~IRDY & WRITE_ONE;
+    wire DATA_READ  = ~DEVSEL & (READ_OP == COMMAND_BUFF) & ~IRDY & ~TRDY & READ_ONE;
     
     
     /*************************************************
@@ -203,33 +260,33 @@ module Device(FRAME,
     always @(posedge CLK or negedge REST)
     begin
         if (~REST) begin
-            INDEX        <= 0;
+            INDEX_WRITE        <= 0;
             DEVICE_READY <= 1;
         end
         else begin
             if (TRANSATION_START)
-                INDEX <= AD[3:2];
+                INDEX_WRITE <= AD[3:2];
             else begin
                 if (DATA_WRITE)
                 begin
-                    if (INDEX < 4)
+                    if (INDEX_WRITE < 4)
                     begin
                         DEVICE_READY <= 1;
                         // Store only the Bytes enableld data
-                        MEM[INDEX] <= (MEM[INDEX] & ~MASK) | (AD & MASK);
+                        MEM[INDEX_WRITE] <= (MEM[INDEX_WRITE] & ~MASK) | (AD & MASK);
                         // Add one to the index to point at the next word
-                        INDEX <= INDEX + 1;
+                        INDEX_WRITE <= INDEX_WRITE + 1;
                     end
                     else begin
-                        // Move 0000the data to temp buffer to be processed by the Device
+                        // Move the data to temp buffer to be processed by the Device
                         // Assert TRDY up during the operation for only one cycle
-                        // then wrap the INDEX to zero
+                        // then wrap the INDEX_WRITE to zero
                         DEVICE_READY   <= 0;
                         TEMP_BUFFER[0] <= MEM[0];
                         TEMP_BUFFER[1] <= MEM[1];
                         TEMP_BUFFER[2] <= MEM[2];
                         TEMP_BUFFER[3] <= MEM[3];
-                        INDEX          <= 0;
+                        INDEX_WRITE          <= 0;
                     end
                 end
             end
@@ -259,8 +316,9 @@ module Device(FRAME,
                 if (DATA_READ) begin
                     // the read opeation doeesn't have side effects
                     // so we only wrap the index to zero
-                    INDEX_READ   <= (INDEX_READ >= 3) ? 0 : INDEX_READ + 1;
                     AD_OUTPUT_EN <= 1;
+                    INDEX_READ   <= (INDEX_READ >= 3) ? 0 : INDEX_READ + 1;
+                    
                 end
                 else begin
                     AD_OUTPUT_EN <= 0;
