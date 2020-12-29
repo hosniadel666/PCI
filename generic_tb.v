c04a1b01c0402ce84e8ce20cae839646255f70c8
Hassan El-shazly <hassanelshazly2010@gmail.com>
2020-12-29 08:38:02 +0200

    fixing multislave bug

--- generic_tb.v
+++ generic_tb.v
@@ -21,12 +21,20 @@ module GenericTestBench();
     
     reg [31:0] AD_REG;
     reg AD_OE;
+
+    parameter BASE_AD           = 32'hFFFF0000;
+    parameter MEM_READ_C        = 4'b0110;
+    parameter MEM_WRITE_C       = 4'b0111;
+    parameter MEM_READ_MUL_C    = 4'b1100;
+    parameter MEM_READ_LINE_C   = 4'b1110;
+    parameter MEM_WRITE_INVAL_C = 4'b1111;
     
     // tri-state the AD lines
     assign AD = AD_OE? AD_REG: 32'hZZZZZZZZ;
     
     // Instantiate the Device
     Device D1(FRAME, CLK, RST, AD, CBE, IRDY, TRDY, DEVSEL, STOP, PAR);
+    Device #(.BASE_AD(32'hFFFFF000)) D2(FRAME, CLK, RST, AD, CBE, IRDY, TRDY, DEVSEL, STOP, PAR);
     
     // Intialize the signals
     initial begin
@@ -35,7 +43,7 @@ module GenericTestBench();
         IRDY   = 1'b1;
         AD_REG = 32'hFFFF_FFFF;
         FRAME  = 1'b1;
-        RST = 0;
+        RST    = 0;
         #1 RST = 1;
     end
     
@@ -49,14 +57,14 @@ module GenericTestBench();
     integer i;
     
     task initialize_data;
-    begin
-        data[0] = 32'h0000_F0F0;
-        data[1] = 32'h0000_F0F1;
-        data[2] = 32'h0000_F0F2;
-        data[3] = 32'h0000_F0F3;
-        data[4] = 32'h0000_F0F4;
-        data[5] = 32'h0000_F0F5;
-    end
+        begin
+            data[0] = 32'h0000_F0F0;
+            data[1] = 32'h0000_F0F1;
+            data[2] = 32'h0000_F0F2;
+            data[3] = 32'h0000_F0F3;
+            data[4] = 32'h0000_F0F4;
+            data[5] = 32'h0000_F0F5;
+        end
     endtask
     
     reg enable_loop = 1;
@@ -82,19 +90,22 @@ module GenericTestBench();
                 #5
                 AD_REG = data[i];
                 i      = i + 1;
-                end else if (TRDY & STOP) begin
+            end
+            else if (TRDY & STOP) begin
                 // device is busy
                 // wait for TRDY
                 $display("Device busy");
                 #5 enable_loop = 1;
-                end else if (~TRDY & ~STOP) begin
+            end
+                else if (~TRDY & ~STOP) begin
                 // device request to end the transction
                 $display("Device requesting disconnet with data");
                 $display("Writing the last word %h", data[i - 1]);
                 #5
                 enable_loop = 0;
                 // finish
-                end else begin
+                end
+            else begin
                 $display("Device requesting retry/disconnet without data");
                 enable_loop = 0;
             end
@@ -102,11 +113,11 @@ module GenericTestBench();
         // the last data transfer
         FRAME = 1;
         #5;
-        if(i == 6)
+        if (i == 6)
             $display("Writing %h", data[i - 1]);
-        #5 IRDY = 1;
-        $display("\nTRANSACTION FINISHED\n");
-        #10; // turn around cycle
+            #5 IRDY = 1;
+            $display("\nTRANSACTION FINISHED\n");
+            #10; // turn around cycle
         
     end
     endtask
@@ -147,18 +158,11 @@ module GenericTestBench();
         end
         FRAME = 1;
         #10
-        IRDY = 1;
+        IRDY  = 1;
         AD_OE = 1;
         $display("\nTRANSACTION FINISHED\n");
     end
     endtask
-
-    parameter BASE_AD           = 32'hFFFF0000;
-    parameter MEM_READ_C        = 4'b0110;
-    parameter MEM_WRITE_C       = 4'b0111;
-    parameter MEM_READ_MUL_C    = 4'b1100;
-    parameter MEM_READ_LINE_C   = 4'b1110;
-    parameter MEM_WRITE_INVAL_C = 4'b1111;
     
     // Starting the Test
     initial begin
@@ -186,8 +190,8 @@ module GenericTestBench();
         CBE    = MEM_READ_C;
         AD_REG = BASE_AD + 1;
         read_data;
-
-         /**** TEST UNSUPPORTED OPERATION ****/
+        
+        /**** TEST UNSUPPORTED OPERATION ****/
         $display("Test Disconnect without data");
         #10
         initialize_data;
